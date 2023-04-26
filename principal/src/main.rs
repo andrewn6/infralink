7aaff3ab2b7f589a967ca91c16b04143f9e61fc9
andrew <andrewnijmeh1@gmail.com>
2023-04-26 09:05:28 -0400

    latest code

--- principal/src/main.rs
+++ principal/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use tonic::transport::Channel;
+use tonic::Request;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>>{
+    let channel = Channel::from_static("http://[::1]:50051")
+        .connect()
+        .await?;
+
+    //let mut client = MemoryService::MemoryServiceServer::new(channel);
+
+    
+    Ok(())
 }
