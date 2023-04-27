c07994e80077a04847c7977bf7882cad09e4a4fb
andrew <andrewnijmeh1@gmail.com>
2023-04-27 14:08:12 -0400

    basic scaling func done

--- principal/src/main.rs
+++ principal/src/main.rs
@@ -1,15 +1,3 @@
-use tonic::transport::Channel;
-use tonic::Request;
-//use worker::memory::MemoryMetadata;
-
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>>{
-    let channel = Channel::from_static("http://[::1]:50051")
-        .connect()
-        .await?;
-
-    //let mut client = MemoryService::MemoryServiceServer::new(channel);
-
-    
-    Ok(())
+fn main() {
+    println!("hello world")
 }
---
+++ principal/src/scale/scale.rs
@@ -0,0 +1,68 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::sync::mpsc;
+use std::thread;
+use std::time::{Duration, SystemTime};
+
+#[derive(Debug)]
+pub struct Metrics {
+    cpu: f64,
+    memory: f64,
+    disk: f64,
+    network: f64,
+    time: SystemTime,
+}
+
+fn main()  {
+    let (tx, rs) = mpsc::channel();
+
+    let request_rate_threshold = 10.0;
+    let latency_threshold = 100.0;
+
+    thread::spawn(move || {
+      let file = File::open("../data/dummy_data.json").unwrap();
+      let reader = BufReader::new(file);
+
+      for line in reader.lines() {
+        if let Ok(json_str) = line {
+          if let Ok(metrics) = serde_json::from_str::<Metrics>(&json_str) {
+            tx.send(metrics).unwrap();
+          }
+        }
+      }  
+    });
+
+    let mut replica_count = 1;
+
+    loop {
+        let mut request_count = 0;
+        let mut latency_sum = 0.0;
+        let mut metrics_count = 0;
+
+        for metrics in rx.try_iter() {
+            if SystemTime::now().duration_since(metrics.time).unwrap().as_secs() > 60 {
+                break;
+            }
+            request_count += 1;
+            latency_sum += metrics.network;
+            metrics_count += 1;
+        }
+
+        if request_count > 0 {
+            let request_rate = request_count as f64 / 60.0;
+            let latency = latency_sum / metrics_count as f64;
+
+            println!("request_rate: {}", request_rate);
+            println!("latency: {}", latency);
+            
+            /* Scale up if request wate/latency exceeds the thresholds */
+            if request_rate > request_rate_threshold && latency < latency_threshold {
+                replica_count += 1;
+            /*/ Scale down if request rate/latency is below the thresholds */
+            } else if replica_count > 1 {
+                replica_count -= 1;
+            }
+        }
+        thread::sleep(Duration::from_millis(1000));
+    }
+}
